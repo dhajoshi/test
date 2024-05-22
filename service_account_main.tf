b0d908fe1570ea7166040917fe26ef3a1ba38bb0
Dhaval <dhajoshi@users.noreply.github.com>
2024-05-22 20:15:58 +0530

    Create service_account_main.tf

---
+++ service_account_main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = var.project_id
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = var.account_id
+  display_name = var.display_name
+}
+
+resource "google_project_iam_member" "service_account_roles" {
+  for_each = toset(var.roles)
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
